5f67788eef60fe9d516b04cbbd73e7dbed3bc287
Uguls <chobozaex@gmail.com>
2023-03-10 12:17:44 +0900

    message

---
+++ web-back/src/main/java/com/grade/webback/service/QuestionService.java
@@ -0,0 +1,40 @@
+package com.grade.webback.service;
+
+import com.grade.webback.DataNotFoundException;
+import com.grade.webback.entity.Question;
+import com.grade.webback.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+        return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
+
+    }
+
+    public void create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q);
+    }
+}
